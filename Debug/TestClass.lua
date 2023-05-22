3258e0a9c9eaf62073e342adc4863a9a67d53836
unknown <wb.yanqihua@game.ntes>
2023-05-22 10:14:03 +0800

    debugclass

--- Debug/TestClass.lua
+++ Debug/TestClass.lua
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-local ParentClass = Class("ParentClass")
-function ParentClass:Func1(...)
-    print("call parent func1:", ...)
-    print("parent data:", self.Data.a)
-end
-
-local ChildClass = Class("ChildClass", ParentClass)
-ChildClass.Data = { a = 1 }
-function ChildClass:Func1(...)
-    self:SuperFunc("Func1", ...)
-    print("call child func1:", ...)
-    print("child data:", self.Data.a)
-end
-
-local childInstance = ChildClass.New()
-childInstance:Func1("参数1", "ABC")
-=======
 local inA = Interface("InterfaceA")
 function inA:InterfaceFuncA()
     print("Call InterfaceFuncA")
@@ -44,4 +26,3 @@ objC:Delete()
 
 local func = objC:ToFunc("FuncC")
 func()
->>>>>>> a5a34bf739c885e7dcd5d551c719a8a8edf7c4a6
